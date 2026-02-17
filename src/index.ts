094aaac895c79a4f4e5df87bf845f07e481fb6cf
Lex <lex.bruynooghe@gmail.com>
2026-02-17 20:26:09 +0100

    first commit, testing workflows

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const greet = (name: string): string => {
+    return "Hello, " + name;
+};
+
+const unusedVar = "I should trigger a warning";
---
+++ tests/index.test.ts
@@ -0,0 +1,5 @@
+import { greet } from '../src/index';
+
+test('greets the user', () => {
+  expect(greet('World')).toBe('Hello, World');
+});
