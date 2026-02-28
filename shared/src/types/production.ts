18e4642f6339a0695baf3c3afcf3d7f56ffa7923
noahvsb <noahvansteenbrugge@gmail.com>
2026-02-28 15:09:27 +0100

    add a production model

--- shared/src/types/production.ts
+++ shared/src/types/production.ts
@@ -1,8 +1,38 @@
 import z from "zod"
+import { AdminSchema } from "./index";
 
-const ProductionSchema = z.object({
+export const ProductionSchema: z.ZodObject = z.object({
+  // metadata
+  created_by: AdminSchema,
+  created_at: z.date(),
+  updated_at: z.date(),
+
   id: z.number(),
-  title: z.string(),
+  vendor_id: z.number(),
+  supertitle: z.json().nullable(),
+  title: z.json(),
+  artist: z.json(),
+  tagline: z.json().nullable(),
+  teaser: z.json().nullable(),
+  description: z.json().nullable(),
+  description_extra: z.json().nullable(),
+  description_2: z.json().nullable(),
+  video_1: z.json().nullable(),
+  video_2: z.json().nullable(),
+  quote: z.json().nullable(),
+  quote_source: z.json().nullable(),
+  programme: z.json().nullable(),
+  info: z.json().nullable(),
+
+  // unnecessary
+  // performer_field: z.string().nullable(),
+  // performer_type: z.string().nullable(),
+  // attendance_mode: z.string().nullable(),
+  // meta_title: z.json().nullable(),
+  // meta_description: z.json().nullable(),
+  // description_short: z.json().nullable(),
+  // eticket_info: z.json().nullable(),
+  // custom_data: z.json().nullable(),
 });
 
 export type Production = z.infer<typeof ProductionSchema>;
