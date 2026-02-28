ee996b2407f10ebb75d5396d34b6ece4eba8e36a
Lex <lex.bruynooghe@gmail.com>
2026-02-28 18:15:28 +0100

    add tag table migrations

---
+++ backend/migrations/004.do.create-tag-tables copy.sql
@@ -0,0 +1,38 @@
+-- Creates tag_type and tag tables
+
+-- ============================================================
+-- TAG TYPE
+-- ============================================================
+CREATE TABLE IF NOT EXISTS tag_type (
+  id          SERIAL          PRIMARY KEY,
+  name        VARCHAR(32)     NOT NULL UNIQUE,
+
+  -- metadata
+  created_by  INT             REFERENCES admin(id) ON DELETE SET NULL,
+  created_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+  updated_by  INT             REFERENCES admin(id) ON DELETE SET NULL,
+  updated_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW()
+);
+
+-- ============================================================
+-- TAG
+-- ============================================================
+CREATE TABLE IF NOT EXISTS tag (
+  id          SERIAL          PRIMARY KEY,
+  name        VARCHAR(32)     NOT NULL,
+  public      BOOLEAN         NOT NULL DEFAULT FALSE, -- false = CMS only, true = Public
+  type_id     INT             NOT NULL REFERENCES tag_type(id) ON DELETE CASCADE,
+  
+
+  -- metadata
+  created_by  INT             REFERENCES admin(id) ON DELETE SET NULL,
+  created_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+  updated_by  INT             REFERENCES admin(id) ON DELETE SET NULL,
+  updated_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+
+  -- Constraints & Indexes
+  CONSTRAINT unique_tag_per_type UNIQUE (name, type_id)
+);
+
+-- Index for faster lookups when filtering productions by tag type
+CREATE INDEX IF NOT EXISTS idx_tag_type_id ON tag(type_id);
---
+++ backend/migrations/004.undo.create-tag-tables.sql
@@ -0,0 +1,3 @@
+-- Drops tag and tag_type tables
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS tag_type;
