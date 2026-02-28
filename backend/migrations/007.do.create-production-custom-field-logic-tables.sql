47aa8a33bcfca57fd9f523b76394aaa39c8599c0
Lex <lex.bruynooghe@gmail.com>
2026-02-28 18:30:55 +0100

    add prod custom field migration files

---
+++ backend/migrations/007.do.create-production-custom-field-logic-tables.sql
@@ -0,0 +1,55 @@
+-- Creates custom field definitions 
+
+
+-- ============================================================
+-- ENUMS
+-- ============================================================
+-- Creating the enum type first so it can be used in table definitions
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'field_types') THEN
+        CREATE TYPE field_types AS ENUM ('number', 'string', 'bool', 'json');
+    END IF;
+END $$;
+
+-- ============================================================
+-- CUSTOM PRODUCTION FIELD DEFINITION
+-- ============================================================
+CREATE TABLE IF NOT EXISTS custom_production_field_definition (
+  id          SERIAL          PRIMARY KEY,
+  name        VARCHAR(64)     NOT NULL,
+  
+  type        field_types     NOT NULL,
+
+  -- metadata
+  created_by  INT             REFERENCES admin(id) ON DELETE SET NULL,
+  created_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+  updated_by  INT             REFERENCES admin(id) ON DELETE SET NULL,
+  updated_at  TIMESTAMPTZ     NOT NULL DEFAULT NOW()
+);
+
+-- ============================================================
+-- PRODUCTION CUSTOM FIELD (The EAV Table)
+-- ============================================================
+CREATE TABLE IF NOT EXISTS production_custom_field (
+  field_id        INT             REFERENCES custom_production_field_definition(id) ON DELETE CASCADE,
+  production_id   INT             REFERENCES production(id) ON DELETE CASCADE,
+  
+  -- The sparse matrix of values
+  value_bool      BOOLEAN,
+  value_number    NUMERIC,
+  value_string    TEXT,           -- TEXT to be safe
+  value_json      JSONB,
+
+  -- metadata
+  created_by      INT             REFERENCES admin(id) ON DELETE SET NULL,
+  created_at      TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+  updated_by      INT             REFERENCES admin(id) ON DELETE SET NULL,
+  updated_at      TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+
+  -- Composite Primary Key: A production can only have ONE value per field definition
+  PRIMARY KEY (field_id, production_id)
+);
+
+-- Index for querying all custom fields for a specific production
+CREATE INDEX IF NOT EXISTS idx_pcf_production_id ON production_custom_field(production_id);
---
+++ backend/migrations/007.undo.create-production-custom-field-logic-tables.sql
@@ -0,0 +1,3 @@
+-- Drops the custom field tables
+DROP TABLE IF EXISTS production_custom_field;
+DROP TABLE IF EXISTS custom_production_field_definition;
