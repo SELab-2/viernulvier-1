b327d2e2383c55c7a594adef108d5c9c9246e699
Thibault <thibault.spiessens@ugent.be>
2026-02-28 22:23:11 +0100

    Initial event model with possible unnecessary fields left as comments

---
+++ shared/src/types/event.ts
@@ -0,0 +1,32 @@
+import z from "zod"
+
+import { AdminSchema, HallSchema } from "./index";
+
+export const EventSchema = z.object({
+    // metadata
+    created_by: AdminSchema,
+    created_at: z.date(),
+    updated_at: z.date(),
+
+    id: z.number(),
+    starts_at: z.date(),
+    ends_at: z.date(),
+    name: z.json(),
+    hall: HallSchema,
+    doors_at: z.date(),
+    box_office_id: z.number(),
+    vendor_id: z.number(),
+    status: z.json(),
+    info: z.json(),
+
+    // unnecessary
+    // intermission_at: z.date().nullable(),
+    // max_tickets_per_order: z.number().nullable(),
+    // uitdatabank_id: z.number().nullable(),
+    // secure: z.boolean().nullable(),
+    // sms_verification: z.boolean().nullable(),
+    // eticket_info: z.json().nullable(),
+    // order_url: z.string().nullable(),
+    // external_order_url: z.json().nullable(),
+
+});
