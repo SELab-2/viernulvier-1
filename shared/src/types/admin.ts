8bb0a487188852608687c68f0a798fe271853947
noahvsb <noahvansteenbrugge@gmail.com>
2026-03-01 14:33:27 +0100

    add metadata schema

--- shared/src/types/admin.ts
+++ shared/src/types/admin.ts
@@ -1,9 +1,10 @@
 import z from "zod"
 
-export const AdminSchema: z.ZodObject = z.object({
-  // metadata
+export const AdminSchema: z.ZodObject<any> = z.object({
+  // metadata (only time this is hardcoded)
   created_by: z.lazy(() => AdminSchema),
   created_at: z.date(),
+  updated_by: z.lazy(() => AdminSchema),
   updated_at: z.date(),
 
   id: z.number(),
