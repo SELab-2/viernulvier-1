c50705d90356344c0c635358b2d9237c57d03a27
Iker <ikerdeaguirre@hotmail.com>
2026-03-05 00:55:54 +0100

    apply Noah's suggestions

--- backend/scripts/migrate.ts
+++ backend/scripts/migrate.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { migrate } from "../src/db/migrate.ts";
 
 migrate(process.argv[2])
-  .then((result) => {
-    console.log("Migration completed:", result);
+  .then(() => {
+    console.log("Migration completed");
     process.exit(0);
   })
   .catch((err) => {
--- migrate-db.sh
+++ migrate-db.sh
@@ -1,5 +1,5 @@
 if [ "$#" -lt 1 ]; then
   docker exec -t viernulvier-backend pnpm run migrate
+else
+  docker exec -t viernulvier-backend pnpm run migrate "$1"
 fi
-
-docker exec -t viernulvier-backend pnpm run migrate "$1"
