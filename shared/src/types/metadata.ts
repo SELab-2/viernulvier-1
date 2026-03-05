94709323f5dfb5adb5b48678598354da4ac9b5ce
noahvsb <noahvansteenbrugge@gmail.com>
2026-03-05 10:36:11 +0100

    made MetadataShape an actual shape

--- shared/src/types/metadata.ts
+++ shared/src/types/metadata.ts
@@ -1,17 +1,17 @@
 import z from "zod";
 import { AdminSchema } from "./index.js";
 
-export const MetadataShape = z.object({
+export const MetadataShape = {
   created_by: AdminSchema,
   created_at: z.date(),
   updated_by: AdminSchema,
   updated_at: z.date(),
-});
+};
 
 // helper function to add a withMeta() function to the Schema's
 export function createSchema<T extends z.ZodRawShape>(shape: T) {
   const base = z.object(shape);
   return Object.assign(base, {
-    withMeta: () => base.extend(MetadataShape.shape),
+    withMeta: () => base.extend(MetadataShape),
   });
 }
