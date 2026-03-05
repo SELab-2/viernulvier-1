866b5149e2ddcb3238b640627e003e37bbb26f97
Thibault <thibault.spiessens@ugent.be>
2026-03-05 21:37:03 +0100

    production in index.js douplicated in merge (went unspotted during merge)

--- shared/src/types/index.ts
+++ shared/src/types/index.ts
@@ -1,6 +1,5 @@
 export * from "./production.js";
 export * from "./admin.js";
 export * from "./metadata.js";
-export * from "./production.js";
 export * from "./hall.js";
 export * from "./event.js";
