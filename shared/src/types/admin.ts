1310b5f87aaffac55481c6f17d845a2a55e393e2
noahvsb <noahvansteenbrugge@gmail.com>
2026-03-05 10:34:20 +0100

    add withMeta() to the models, so we don't need to exclude all the metadata fields each time we don't need them

--- shared/src/types/admin.ts
+++ shared/src/types/admin.ts
@@ -1,17 +1,22 @@
 import z from "zod"
 
-export const AdminSchema: z.ZodObject<any> = z.object({
-  // METADATA
-  // only time this is hardcoded to avoid infinite import loops
-  // normally you would use ...MetadataSchema.shape instead of hardcoding
-  created_by: z.lazy(() => AdminSchema),
-  created_at: z.date(),
-  updated_by: z.lazy(() => AdminSchema),
-  updated_at: z.date(),
-
+export const AdminBase = z.object({
   id: z.number().int().positive(),
   username: z.string().max(32),
   profile_picture: z.url().max(2048).nullable(),
 });
 
+export const AdminSchema = Object.assign(AdminBase, {
+  withMeta: () => AdminBase.extend({
+    // METADATA
+    // only time this is hardcoded to avoid infinite import loops
+    // normally you would create the schema using createSchema() from metadata.ts instead of z.object()
+    created_by: z.lazy(() => AdminSchema),
+    created_at: z.date(),
+    updated_by: z.lazy(() => AdminSchema),
+    updated_at: z.date(),
+  }),
+});
+
 export type Admin = z.infer<typeof AdminSchema>;
+export type AdminWithMeta = z.infer<ReturnType<typeof AdminSchema.withMeta>>;
--- shared/src/types/metadata.ts
+++ shared/src/types/metadata.ts
@@ -1,9 +1,17 @@
 import z from "zod";
 import { AdminSchema } from "./index.js";
 
-export const MetadataSchema = z.object({
+export const MetadataShape = z.object({
   created_by: AdminSchema,
   created_at: z.date(),
   updated_by: AdminSchema,
   updated_at: z.date(),
 });
+
+// helper function to add a withMeta() function to the Schema's
+export function createSchema<T extends z.ZodRawShape>(shape: T) {
+  const base = z.object(shape);
+  return Object.assign(base, {
+    withMeta: () => base.extend(MetadataShape.shape),
+  });
+}
