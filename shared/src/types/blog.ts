03f4c7ec3ba7793e77e7276a206822f864b5fedd
Thibault <thibault.spiessens@ugent.be>
2026-03-05 01:20:02 +0100

    Different way to use metadata

--- shared/src/types/blog.ts
+++ shared/src/types/blog.ts
@@ -1,16 +1,20 @@
 import z from "zod"
 
-import { AdminSchema, MetadataSchema } from "./index";
+import { MetadataSchema } from "./index";
 
-export const BlogSchema = MetadataSchema.extend({
-    id: z.number(),
+export const BlogSchema = z.object({
+    ...MetadataSchema.shape,
+
+    id: z.number().int().positive(),
     name: z.string(),
 });
 
 export type Blog = z.infer<typeof BlogSchema>;
 
-export const BlogPostSchema = MetadataSchema.extend({
-    id: z.number(),
+export const BlogPostSchema = z.object({
+    ...MetadataSchema.shape,
+
+    id: z.number().int().positive(),
     content: z.string(),
     blog: BlogSchema,
 });
