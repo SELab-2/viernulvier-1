e107433ab3fe8941aafb40a82be9dcfbbcff6fa3
noahvsb <noahvansteenbrugge@gmail.com>
2026-03-05 13:35:03 +0100

    use createSchema for AdminSchema

--- shared/src/types/admin.ts
+++ shared/src/types/admin.ts
@@ -1,30 +1,18 @@
 import z from "zod";
-import { foreignKey, primaryKey } from "./helpers";
+import { primaryKey } from "./helpers";
+import { createSchema, MetadataShape } from ".";
 
-export const AdminBase = z.object({
+export const AdminBase = {
   id: primaryKey(),
   username: z.string().max(32),
   profile_picture: z.url().max(2048).nullable(),
-});
-
-export const AdminSchema = Object.defineProperty(AdminBase, 'withMeta', {
-  value: () => AdminBase.extend({
-    created_by: foreignKey(() => AdminSchema),
-    created_at: z.date(),
-    updated_by: foreignKey(() => AdminSchema),
-    updated_at: z.date(),
-  }),
-  writable: false,
-  enumerable: false,
-  configurable: false,
-}) as typeof AdminBase & {
-  withMeta: () => ReturnType<typeof AdminBase.extend<{
-    created_by: ReturnType<typeof foreignKey>,
-    created_at: z.ZodDate,
-    updated_by: ReturnType<typeof foreignKey>,
-    updated_at: z.ZodDate,
-  }>>;
 };
+
+type AdminSchemaType = z.ZodObject<typeof AdminBase> & {
+  withMeta: () => z.ZodObject<typeof AdminBase & typeof MetadataShape>;
+};
+
+export const AdminSchema: AdminSchemaType = createSchema(AdminBase);
 
 export type Admin = z.infer<typeof AdminSchema>;
 export type AdminWithMeta = z.infer<ReturnType<typeof AdminSchema.withMeta>>;
