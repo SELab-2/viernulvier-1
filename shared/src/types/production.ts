46dfc3f7bd52e32d46fa65dec1483f0b3caf40fa
Noah Van Steenbrugge <161894387+noahvsb@users.noreply.github.com>
2026-03-06 18:39:46 +0100

    fix schema typing (#166)

--- shared/src/types/production.ts
+++ shared/src/types/production.ts
@@ -3,7 +3,7 @@ import { EventSchema, TagSchema, createSchema } from "./index.js";
 import type { SchemaWithMeta } from "./index.js";
 import { foreignKey, primaryKey, languageMap } from "./helpers.js";
 
-export const ProductionSchema: SchemaWithMeta<any> = createSchema({
+export const ProductionSchema: SchemaWithMeta<Record<string, z.ZodTypeAny>> = createSchema({
   id: primaryKey(),
   vendor_id: z.int().nonnegative(),
   box_office_id: z.int().nonnegative(),
