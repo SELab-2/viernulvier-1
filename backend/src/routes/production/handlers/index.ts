b5b7194c0f9e2c17206ec41b38abaef61ccdd551
lucas <lucasopdebeeck@gmail.com>
2026-03-06 22:46:46 +0100

    add index file

---
+++ backend/src/routes/production/handlers/index.ts
@@ -0,0 +1,5 @@
+export { fetchProduction, fetchProductions, getProductionById } from "./fetch.js";
+export { createProduction } from "./create.js";
+export { editProduction } from "./edit.js";
+export { bulkEditProductions } from "./bulk-edit.js";
+export { deleteProduction } from "./delete.js";
