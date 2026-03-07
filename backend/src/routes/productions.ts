750f4fd67917f9ec22ba319be8bd10c4f44a9c10
Iker <ikerdeaguirre@hotmail.com>
2026-03-07 21:45:39 +0100

    example of how to use the buildQuery and parseParams.

--- backend/src/routes/productions.ts
+++ backend/src/routes/productions.ts
@@ -1,27 +1,37 @@
 import type { FastifyInstance } from "fastify";
-import type { Production } from "@viernulvier/shared/types/production.js";
+import {
+  ProductionSchema,
+  type Production,
+} from "@viernulvier/shared/types/production.js";
+import { buildQuery, parseParams } from "./helpers.js";
+import z from "zod";
+import { stringToInt } from "@viernulvier/shared/types/helpers.js";
 
 /**
  * Defines the api requests for /api/productions
  * @alpha
  * @param server - The fastify server instance on which this request will be served
  */
-
 export default function productionRoutes(server: FastifyInstance) {
   server.get("/api/production/:id", async (request) => {
-    const { id } = request.params as { id: string };
-    const result = await server.pg.query<Production>(
+    const { id } = parseParams(request, z.object({ id: stringToInt }));
+    const result = await buildQuery(
+      server,
       "SELECT id FROM productions WHERE id=$1",
-      [id],
-    );
+      z.tuple([ProductionSchema.shape.id]),
+      ProductionSchema,
+    )(id);
 
-    return result.rows[0];
+    return result[0];
   });
   server.get("/api/production", async () => {
-    const result = await server.pg.query<Production>(
+    const result = await buildQuery(
+      server,
       "SELECT id FROM productions",
-    );
+      z.tuple([]),
+      ProductionSchema.pick({ id: true }),
+    )();
 
-    return result.rows;
+    return result;
   });
 }
