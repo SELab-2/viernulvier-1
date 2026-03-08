bd7fac8609f018c3040e1c3d4a4f013b01db33d7
Willem DC <willem.de.craene.wss@gmail.com>
2026-03-08 13:29:09 +0100

    Add tag route handler exports

---
+++ backend/src/routes/handlers/tag/index.ts
@@ -0,0 +1,4 @@
+export { fetchTag, fetchTags, getTagById } from "./fetch.js";
+export { createTag } from "./create.js";
+export { editTag } from "./edit.js";
+export { deleteTag } from "./delete.js";
