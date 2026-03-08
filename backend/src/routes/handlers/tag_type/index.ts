4d54be27343aa838d6718f2d7a2e858330b6670f
Willem DC <willem.de.craene.wss@gmail.com>
2026-03-08 13:31:11 +0100

    Add index file for tag type route handlers

---
+++ backend/src/routes/handlers/tag_type/index.ts
@@ -0,0 +1,4 @@
+export { fetchTagType, fetchTagTypes } from "./fetch.js";
+export { createTagType } from "./create.js";
+export { editTagType } from "./edit.js";
+export { deleteTagType } from "./delete.js";
