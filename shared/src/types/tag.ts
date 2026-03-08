3d63c6ba3d2da5416f099d0d8dace2cfb5c41d75
Iker <ikerdeaguirre@hotmail.com>
2026-03-08 21:45:34 +0100

    cleaner imports

--- shared/src/types/tag.ts
+++ shared/src/types/tag.ts
@@ -6,7 +6,7 @@ import {
   languageMap,
   type ForeignKey,
 } from "./helpers.js";
-import { createSchema } from "./index.js";
+import {createSchema} from "./metadata.js"
 
 export const TagTypeSchema = createSchema({
   id: primaryKey(),
