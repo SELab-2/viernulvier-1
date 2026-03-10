58be61e478ac32b720424431bee9ecb8e1844c24
Iker <ikerdeaguirre@hotmail.com>
2026-03-10 20:45:40 +0100

    Add a helper.

--- backend/src/routes/helpers.ts
+++ backend/src/routes/helpers.ts
@@ -343,3 +343,38 @@ export function getMetadata(request: FastifyRequest) {
     current_time: new Date(),
   };
 }
+
+type schemaDef = {
+  body?: z.ZodType;
+  params?: z.ZodType;
+  response?: Record<number, z.ZodType>;
+  default?: z.ZodType;
+  description?: string;
+  tags?: string[];
+};
+
+export class RequestSchema {
+  private _schema: schemaDef;
+  constructor(schema: schemaDef = {}) {
+    this._schema = schema;
+    return this;
+  }
+
+  public extend(schema: schemaDef) {
+    const newSchema = {
+      ...this._schema,
+      ...schema,
+    };
+    newSchema.response = {
+      ...this._schema.response,
+      ...schema.response,
+    };
+    newSchema.tags = [...(this._schema.tags ?? []), ...(schema.tags ?? [])];
+    this._schema = newSchema;
+    return this;
+  }
+
+  public use() {
+    return this._schema;
+  }
+}
