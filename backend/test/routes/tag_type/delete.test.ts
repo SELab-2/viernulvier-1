e0598860dbca53a42ce49db3aeb3725d8869e243
Willem DC <willem.de.craene.wss@gmail.com>
2026-03-11 23:10:21 +0100

    Update backend/test/routes/tag_type/delete.test.ts
    Co-authored-by: Simon-VdB <65555254+Simon-VdB@users.noreply.github.com>

--- backend/test/routes/tag_type/delete.test.ts
+++ backend/test/routes/tag_type/delete.test.ts
@@ -58,7 +58,7 @@ describe("Delete tag_type", () => {
     const response = await server.inject({
       method: "DELETE",
       cookies: { session: sessionCookie },
-      url: `/api/v1/tag/type/999`,
+      url: `/api/v1/tags/type/999`,
     });
 
     expect(response.statusCode).toBe(404);
