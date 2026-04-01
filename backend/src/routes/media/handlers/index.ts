a2dc5586f628b673409754870927bbb8c397f1ce
Lex <lex.bruynooghe@gmail.com>
2026-04-01 17:35:40 +0200

    index for media endpoint

---
+++ backend/src/routes/media/handlers/index.ts
@@ -0,0 +1,11 @@
+export {
+  fetchImagesByProduction,
+  fetchImage,
+  fetchImageWithMeta,
+  fetchCropsByImage,
+  fetchCropByType,
+} from "./fetch.js";
+export { createImage, createCrops } from "./create.js";
+// export { editImage, editCrop } from "./edit.js";
+// export { replaceImage, replaceCrop } from "./replace.js";
+// export { deleteImage, deleteCrop } from "./delete.js";
