3058ce8e2b6afc20118b6eb9ee8e120f5a51fbe2
Lex <lex.bruynooghe@gmail.com>
2026-04-02 11:52:56 +0200

    delete en replace done

--- backend/src/routes/media/handlers/index.ts
+++ backend/src/routes/media/handlers/index.ts
@@ -7,5 +7,5 @@ export {
 } from "./fetch.js";
 export { createImage, createCrops } from "./create.js";
 export { editImage, editCrop } from "./edit.js";
-// export { replaceImage, replaceCrop } from "./replace.js";
-// export { deleteImage, deleteCrop } from "./delete.js";
+export { replaceImage, replaceCrop } from "./replace.js";
+export { deleteImage, deleteCrop } from "./delete.js";
