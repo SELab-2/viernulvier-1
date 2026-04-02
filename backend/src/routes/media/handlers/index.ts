b6e7a3a66cd1296ee9b7f231b429e152a948edce
Lex <lex.bruynooghe@gmail.com>
2026-04-02 11:43:22 +0200

    we can export these now, its implemented

--- backend/src/routes/media/handlers/index.ts
+++ backend/src/routes/media/handlers/index.ts
@@ -6,6 +6,6 @@ export {
   fetchCropByType,
 } from "./fetch.js";
 export { createImage, createCrops } from "./create.js";
-// export { editImage, editCrop } from "./edit.js";
+export { editImage, editCrop } from "./edit.js";
 // export { replaceImage, replaceCrop } from "./replace.js";
 // export { deleteImage, deleteCrop } from "./delete.js";
