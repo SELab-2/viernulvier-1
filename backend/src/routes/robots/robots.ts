ddde5e0bea9a4897a01d82f156449bb2c2497b38
Lex <166119555+Acher0s@users.noreply.github.com>
2026-04-30 07:43:47 +0200

    trigger update on staging

--- backend/src/routes/robots/robots.ts
+++ backend/src/routes/robots/robots.ts
@@ -4,7 +4,6 @@ export default async function robotsRoutes(server: FastifyInstance) {
   server.get("/robots.txt", async (_, reply) => {
     const content = `User-agent: *
 Disallow: /`;
-
     reply
       .header("Content-Type", "text/plain")
       .send(content);
