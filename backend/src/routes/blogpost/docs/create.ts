14f5a807827a9982228f3bd6e9e425283f1f1039
Iker <ikerdeaguirre@hotmail.com>
2026-05-06 12:20:43 +0200

    Use new schema

--- backend/src/routes/blogpost/docs/create.ts
+++ backend/src/routes/blogpost/docs/create.ts
@@ -1,11 +1,18 @@
-import { protectedRequest, RequestBody, RequestDescription, requestSchema } from "@/docs/helpers.js";
-import { CreateBlogPostBodySchema } from "../handlers/create.js";
+import {
+  protectedRequest,
+  RequestBody,
+  RequestDescription,
+  requestSchema,
+} from "@/docs/helpers.js";
+import { CreateBlogPostInputSchema } from "../handlers/create.js";
 import { returnsBlogPost, sharedRequestSchema } from "./shared.js";
 
 export const createBlogPostDocs = requestSchema(
   sharedRequestSchema,
-  new RequestBody(CreateBlogPostBodySchema),
+  new RequestBody(CreateBlogPostInputSchema),
   returnsBlogPost,
   protectedRequest,
-  new RequestDescription("Creates a new blogpost and returns the created blogpost."),
+  new RequestDescription(
+    "Creates a new blogpost and returns the created blogpost.",
+  ),
 );
