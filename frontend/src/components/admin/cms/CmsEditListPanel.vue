0706087a8f2d58f270b6dbf8b79dea74e9839627
noahvsb <noahvansteenbrugge@gmail.com>
2026-05-19 18:41:02 +0200

    fix multiple panels being open at the same time

--- frontend/src/components/admin/cms/CmsEditListPanel.vue
+++ frontend/src/components/admin/cms/CmsEditListPanel.vue
@@ -1,100 +1,102 @@
 <template>
-  <aside v-if="panel" class="cms-side-panel">
-    <div class="cms-side-header">
-      <div class="min-w-0">
-        <h2 class="text-lg font-semibold text-ink-primary">
-          {{ panel.label }}
-        </h2>
+  <div v-if="panel" class="cms-side-overlay" @click.self="emit('close')">
+    <aside class="cms-side-panel">
+      <div class="cms-side-header">
+        <div class="min-w-0">
+          <h2 class="text-lg font-semibold text-ink-primary">
+            {{ panel.label }}
+          </h2>
+        </div>
+
+        <button
+          type="button"
+          class="cms-side-close"
+          @click="emit('close')"
+        >
+          {{ t("cms.panel.close") }}
+        </button>
       </div>
 
-      <button
-        type="button"
-        class="cms-side-close"
-        @click="emit('close')"
-      >
-        {{ t("cms.panel.close") }}
-      </button>
-    </div>
-
-    <div class="cms-side-body">
-      <label class="cms-list-create">
-        <form
-          class="cms-list-entry"
-          @submit.prevent="addSingle"
-        >
-          <input
-            ref="singleInputEl"
-            v-model="singleDraft"
-            type="text"
-            inputmode="numeric"
-            class="cms-text-input"
-            data-testid="edit-list-panel-single-input"
-          />
-
-          <button
-            type="submit"
-            class="cms-list-add"
+      <div class="cms-side-body">
+        <label class="cms-list-create">
+          <form
+            class="cms-list-entry"
+            @submit.prevent="addSingle"
           >
-            +
-          </button>
-        </form>
+            <input
+              ref="singleInputEl"
+              v-model="singleDraft"
+              type="text"
+              inputmode="numeric"
+              class="cms-text-input"
+              data-testid="edit-list-panel-single-input"
+            />
 
-        <p v-if="singleError" class="text-sm text-red-700">
-          {{ singleError }}
-        </p>
-      </label>
-
-      <div class="cms-side-field">
-        <div
-          v-if="localItems.length > 0"
-          class="cms-list-flairs"
-        >
-          <div class="flex flex-wrap gap-2">
-            <span
-              v-for="id in localItems"
-              :key="id"
-              class="cms-list-flair"
-              data-testid="edit-list-panel-tag"
+            <button
+              type="submit"
+              class="cms-list-add"
             >
-              <a
-                v-if="panel.urlBase"
-                :href="`${panel.urlBase}/${id}`"
-                class="cms-list-flair-link"
-                target="blank"
-              >
-                {{ id }}
-              </a>
-              <span v-else>{{ id }}</span>
+              +
+            </button>
+          </form>
 
-              <button
-                type="button"
-                class="cms-list-flair-remove"
-                @click="remove(id)"
+          <p v-if="singleError" class="text-sm text-red-700">
+            {{ singleError }}
+          </p>
+        </label>
+
+        <div class="cms-side-field">
+          <div
+            v-if="localItems.length > 0"
+            class="cms-list-flairs"
+          >
+            <div class="flex flex-wrap gap-2">
+              <span
+                v-for="id in localItems"
+                :key="id"
+                class="cms-list-flair"
+                data-testid="edit-list-panel-tag"
               >
-                ×
-              </button>
-            </span>
+                <a
+                  v-if="panel.urlBase"
+                  :href="`${panel.urlBase}/${id}`"
+                  class="cms-list-flair-link"
+                  target="blank"
+                >
+                  {{ id }}
+                </a>
+                <span v-else>{{ id }}</span>
+
+                <button
+                  type="button"
+                  class="cms-list-flair-remove"
+                  @click="remove(id)"
+                >
+                  ×
+                </button>
+              </span>
+            </div>
           </div>
         </div>
+
+        <p v-if="saveError" class="text-sm text-red-700">
+          {{ saveError }}
+        </p>
       </div>
 
-      <p v-if="saveError" class="text-sm text-red-700">
-        {{ saveError }}
-      </p>
-    </div>
-
-    <div class="cms-side-footer">
-      <button
-        type="button"
-        class="cms-side-save"
-        :disabled="isSaving"
-        data-testid="edit-list-panel-save"
-        @click="emit('save')"
-      >
-        {{ isSaving ? t("general.saving") : t("cms.panel.save") }}
-      </button>
-    </div>
-  </aside>
+      <div class="cms-side-footer">
+        <button
+          type="button"
+          class="cms-side-save"
+          :disabled="isSaving"
+          data-testid="edit-list-panel-save"
+          @click="emit('save')"
+        >
+          {{ isSaving ? t("general.saving") : t("cms.panel.save") }}
+        </button>
+      </div>
+    </aside>
+  </div>
 </template>
 
 <script setup lang="ts">
