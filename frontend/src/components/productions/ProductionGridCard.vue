bbf6af71cb947aa6ca81056e850e21c1e6609a88
Thibault <thibault.spiessens@ugent.be>
2026-05-19 18:49:57 +0200

    forgot to save 2 tabs

--- frontend/src/components/productions/ProductionGridCard.vue
+++ frontend/src/components/productions/ProductionGridCard.vue
@@ -102,7 +102,7 @@ import type { ProductionWithBackwardsRefs } from "@viernulvier/shared";
 import type { SupportedLang } from "@/i18n";
 import { RouteNames } from "@/router/routeNames";
 import EventCalendarIcon from "@/components/icons/EventCalendarIcon.vue";
-import { localizeOrEmpty } from "@/utils/language-utils";
+import { localizeWithFallback, localizeOrEmpty } from "@/utils/language-utils";
 import type { ProductionDateSummary } from "@/utils/productionsOverview";
 import type { ProductionTagChip } from "@/utils/tagDisplay";
 import { useDarkMode } from "@/composables/useDarkMode";
@@ -176,10 +176,10 @@ const staggerDelayMs = computed(() =>
 );
 
 const title = computed(() =>
-  localizeOrEmpty(props.production.title, locale.value as SupportedLang),
+  localizeWithFallback(props.production.title, (map) => localizeOrEmpty(map, locale.value as SupportedLang)),
 );
 const artist = computed(() =>
-  localizeOrEmpty(props.production.artist, locale.value as SupportedLang),
+  localizeWithFallback(props.production.artist, (map) => localizeOrEmpty(map, locale.value as SupportedLang)),
 );
 </script>
 
